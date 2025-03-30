3b1eea419cc979454a73d82094c4ca1444c44942
Bianca Mori Zegarra <z5534123@ad.unsw.edu.au>
2025-03-31 01:56:39 +1100

    Completed landing page according to design on figma

--- frontend/src/pages/Landing/Landing.tsx
+++ frontend/src/pages/Landing/Landing.tsx
@@ -1,8 +1,29 @@
- 
+import "./Landing.css";
+import { useNavigate } from 'react-router-dom';
+
+<link href="https://fonts.cdnfonts.com/css/kaushan-script" rel="stylesheet"></link>
 function Landing() {
+    const navigate = useNavigate();
+    const clickMe = () => {
+        navigate('/Home');
+    }
+  
     return (
-        <div>
-            <h1>🛬 Landing Page :D</h1>
+        <div className="background">
+            <img src="/bubble.png" alt="bubble" className="bubble-right-up"/>
+            <img src="/bubble.png" alt="bubble" className="bubble-right-down"/>
+            <img src="/bubble.png" alt="bubble" className="bubble-center-down"/>
+            <img src="/bubble.png" alt="bubble" className="bubble-center-middle"/>
+            <img src="/bubble.png" alt="bubble" className="bubble-left-down"/>
+            <img src="/bubble.png" alt="bubble" className="bubble-left-middle"/>
+            <img src="/bubble.png" alt="bubble" className="bubble-left-top"/>
+            <div className="container">
+                <h1 className="title">Bubbles</h1>
+                <button className ="green-button" onClick={clickMe} >
+                    Get Started
+                    <img src="/arrow.png" alt="arrow" className="arrow-button"/>
+                </button>
+            </div>
         </div>
     );
 }
