50a29bb7c41b8d1f1ce0849a91c802eeb6e895ef
Sophie Fox <z5590854@ad.unsw.edu.au>
2025-04-06 15:25:50 +1000

    Complete boxes for subjects

--- frontend/src/components/SubjectPanel/SubjectPanel.tsx
+++ frontend/src/components/SubjectPanel/SubjectPanel.tsx
@@ -1,10 +1,25 @@
+import React, { useState } from 'react';
+import './SubjectPanel.css';
 
 function SubjectPanel() {
-    return (
-      <div>
-        <h2>I'm a SubjectPanel :]</h2>
-      </div>
-    );
-  }
-  
-  export default SubjectPanel;
+  const [subjects] = useState([
+    { id: 1, text: "COMP1511" },
+    { id: 2, text: "COMP1521" },
+    { id: 3, text: "COMP1531" },
+    { id: 4, text: "COMP2511" },
+    { id: 5, text: "COMP2521" }
+  ]);
+
+
+  return (
+    <div className="grid-container">
+      {subjects.map(subject => (
+        <div key={subject.id} className="subject-box">
+          {subject.text}
+        </div>
+      ))}
+    </div>
+  );
+}
+
+export default SubjectPanel;
