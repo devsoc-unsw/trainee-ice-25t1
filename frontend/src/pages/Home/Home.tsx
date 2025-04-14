1aea8e58572b12525d1921dee61e103d8b4d3020
EvaHaverkort <ealhaverkort@gmail.com>
2025-04-14 15:02:28 +1000

    Integrated the search bar with the subject panel instead of the placeholder list

--- frontend/src/pages/Home/Home.tsx
+++ frontend/src/pages/Home/Home.tsx
@@ -1,12 +1,17 @@
+import React, { useState } from "react";
 import SearchBar from "../../components/SearchBar/Searchbar";
 import SubjectPanel from "../../components/SubjectPanel/SubjectPanel";
 
+
+
 function Home() {
+    const [inputText, setInputText] = useState("");
+
     return (
         <div>
             <h1>🏠 Home Page :D</h1>
-            <SearchBar/>
-            <SubjectPanel/>
+            <SearchBar  inputText={inputText} setInputText={setInputText} />
+            <SubjectPanel inputText={inputText} />
         </div>
     );
 }
