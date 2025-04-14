6c7933a6711520cb28e7024a254a84cc996189bc
EvaHaverkort <ealhaverkort@gmail.com>
2025-04-14 18:02:11 +1000

    trying to put prereq skills and subjects in a 2-col grid

--- frontend/src/pages/CourseInfo/CourseInfo.tsx
+++ frontend/src/pages/CourseInfo/CourseInfo.tsx
@@ -1,8 +1,18 @@
- 
+
 function CourseInfo() {
     return (
-        <div>
-            <h1>📖 CourseInfo Page :D</h1>
+        <div >
+            <h1>📖 Course title</h1>
+            <h2>Course Outline</h2>
+            <div className="grid grid-cols-2 gap-4">
+              <div>
+                <h2>Prerequisite Skills</h2>
+              </div>
+              <div>
+                <h2>Prerequisite Subjects</h2>
+              </div>
+            </div>
+            <h2>Timeline</h2>
         </div>
     );
 }
