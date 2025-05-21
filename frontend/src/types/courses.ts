dd1029da4a5ebc5dd7b190546022332e2548afa1
Sophie Fox <z5590854@ad.unsw.edu.au>
2025-05-21 23:26:47 +1000

    Data from JSON

---
+++ frontend/src/types/courses.ts
@@ -0,0 +1,22 @@
+export type Course = {
+    title: string;
+    code: string;
+    description: string;
+    UOC: number;
+    level: number;
+    study_level: string;
+    school: string;
+    faculty: string;
+    campus: string;
+    equivalents: Record<string, unknown>;
+    exclusions: Record<string, unknown>;
+    terms: string[];
+    gen_ed: boolean;
+    raw_requirements: string;
+    is_multiterm: boolean;
+  };
+  
+  export type CourseData = {
+    [courseCode: string]: Course;
+  };
+  
