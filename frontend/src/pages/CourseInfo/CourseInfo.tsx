5caf06349035f4185708d6240b06bd1014d8737d
BobHell0 <anujan.cenan@gmail.com>
2025-05-23 00:57:39 +1000

    made the timeline 11 weeks - used query parameters to dynamically show subject info

--- frontend/src/pages/CourseInfo/CourseInfo.tsx
+++ frontend/src/pages/CourseInfo/CourseInfo.tsx
@@ -4,22 +4,37 @@ import PrerequisiteSubjects from '../../components/CourseInfo/PrerequisiteSubjec
 import Timeline from '../../components/CourseInfo/Timeline';
 import CourseDataRaw from './Data/CourseData.json'
 import { CourseData } from '../../types/courses.ts'
+
+import { useLocation } from 'react-router-dom';
+
 import './CourseInfo.css';
 
+const useQuery = () => {
+  return new URLSearchParams(useLocation().search);
+};
+
 function CourseInfo() {
+  const query = useQuery();
+
+  var code = query.get('code');
+  if (!code)
+    code = 'COMP1511';
+
+  code = code.toLocaleUpperCase()
+
   const courseData = CourseDataRaw as CourseData;
-  const course = courseData["COMP1521"];
+  const course = courseData[code];
   return (
     <div>
-      <h1>📖 COMP1521</h1>
+      <h1>📖{code}</h1>
       <CourseDescription description={course.description} />
 
       <div className="prerequisites_grid">
-        <PrerequisiteSkills />
+        <PrerequisiteSkills code={code}/>
         <PrerequisiteSubjects prereq={course.raw_requirements} />
       </div>
 
-      <Timeline />
+      <Timeline code={code}/>
     </div>
   );
 }
