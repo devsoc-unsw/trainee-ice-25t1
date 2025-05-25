109e33bfbeb68dd915ca01054297e21cc687df89
Tet Nay Lin <tetnaylin168@gmail.com>
2025-05-26 01:15:14 +1000

    Remove unnecessary bullet points

--- frontend/src/components/CourseInfo/PrerequisiteSkills.tsx
+++ frontend/src/components/CourseInfo/PrerequisiteSkills.tsx
@@ -1,6 +1,11 @@
+import { styled } from "@mui/material";
+
 interface PrerequisiteSkillsProps {
   info?: string[];
 }
+const StyledLi = styled("li")(() => ({
+  listStyleType: "none"
+}))
 
 function PrerequisiteSkills({ info }: PrerequisiteSkillsProps) {
   console.log(info)
@@ -9,9 +14,9 @@ function PrerequisiteSkills({ info }: PrerequisiteSkillsProps) {
       <h2>Prerequisite Skills</h2>
       <ul>
         {info && info.length > 0 ? (
-          info.map((item, index) => <li key={index}>{item}</li>)
+          info.map((item, index) => <StyledLi key={index}>{item}</StyledLi>)
         ) : (
-          <li>No prerequisite skills listed.</li>
+          <StyledLi>No prerequisite skills listed.</StyledLi>
         )}
       </ul>
     </div>
