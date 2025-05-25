bcaaea86775cc4275f84f9cd869125e842f5d76c
Tet Nay Lin <tetnaylin168@gmail.com>
2025-05-25 18:01:46 +1000

    Change icon, add logo and easter egg

--- frontend/src/components/NavBar/NavBar.tsx
+++ frontend/src/components/NavBar/NavBar.tsx
@@ -1,9 +1,13 @@
+import { Tooltip } from "@mui/material";
 import "./NavBar.css"
 
 const Navbar = () => {
     return (
         <div>
             <nav>
+                <Tooltip title="Hi! Pls save meee">
+                    <img src="/funny_lil_pokemon.png" alt="logo" className="logo"/>
+                </Tooltip>
                 <ul>
                     <li><a href="/">Landing🛬</a></li>
                     <li><a href="/home">Home🏠</a></li>
