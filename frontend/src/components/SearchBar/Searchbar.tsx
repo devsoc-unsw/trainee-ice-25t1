fbb5c9d2814d0849701380e3a3b35ddf21ad51d4
Tet Nay Lin <tetnaylin168@gmail.com>
2025-05-25 18:25:22 +1000

    Improve search bar visuals

--- frontend/src/components/SearchBar/Searchbar.tsx
+++ frontend/src/components/SearchBar/Searchbar.tsx
@@ -1,6 +1,7 @@
 import React, { useState } from "react";
 import TextField from "@mui/material/TextField";
 import List from "../List.tsx"
+import { Paper } from "@mui/material";
 
 type SearchBarProps = {
   inputText: string;
@@ -14,16 +15,28 @@ function SearchBar({ inputText, setInputText }: SearchBarProps) {
 
   return (
     <div>
-      <h2>Search courses below :)</h2>
+      {/* <h2>Search courses below :)</h2> */}
       {/* for debugging:
       <p>Searching for: {inputText}</p> */}
-      <TextField
-        id="outlined-basic"
-        onChange={handleInputChange}
-        variant="outlined"
-        fullWidth
-        label="Search"
-      />
+      <Paper
+        sx={{
+          /* For Glassmorphism */
+          background: "rgba(255, 255, 255, 0.8)",
+          backdropFilter: "blur(15px)",
+          border: "1px solid rgba(255, 255, 255, 5)",
+          boxShadow: "0 8px 32px rgba(0, 0, 0, 0.1)",
+        }}
+      >
+        <TextField
+          id="filled-search"
+          onChange={handleInputChange}
+          variant="filled"
+          color="success"
+          fullWidth
+          label="Search for a course..."
+        />
+      </Paper>
+      
     </div>
   );
 }
