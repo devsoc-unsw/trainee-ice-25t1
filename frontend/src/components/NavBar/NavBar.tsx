2a15a8099d46600cc0185e168ab35b01be793d3e
Tet Nay Lin <tetnaylin168@gmail.com>
2025-05-25 21:24:29 +1000

    Add navigate to landing page upon clicking on logo in the navbar

--- frontend/src/components/NavBar/NavBar.tsx
+++ frontend/src/components/NavBar/NavBar.tsx
@@ -1,15 +1,20 @@
 import { Tooltip } from "@mui/material";
 import "./NavBar.css"
+import { useNavigate } from "react-router-dom";
 
 const Navbar = () => {
+    const navigate = useNavigate();
+
     return (
         <div>
             <nav>
-                <Tooltip title="Hi! Pls save meee">
-                    <img src="/funny_lil_pokemon.png" alt="logo" className="logo"/>
+                {/* Clicking on the logo leads to the landing page */}
+                <Tooltip title="Hi! Pls save meee" arrow>
+                    <img src="/funny_lil_pokemon.png" alt="logo" className="logo" onClick={() => navigate(`/`)}/>
                 </Tooltip>
+                <h1 className="logo-title" onClick={() => navigate(`/`)}>Bubbles</h1>
                 <ul>
-                    <li><a href="/">Landing🛬</a></li>
+                    {/* <li><a href="/">Landing🛬</a></li> */}
                     <li><a href="/home">Home🏠</a></li>
                     <li><a href="/course-info">Course Info🤔</a></li>
                     <li><a href="/course-compare">Course Compare📖</a></li>
