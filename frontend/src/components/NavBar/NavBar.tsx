ecc308cf80155110611f8b8f6116d1078876be8a
Tet Nay Lin <tetnaylin168@gmail.com>
2025-05-25 17:42:22 +1000

    Improve NavBar styling

--- frontend/src/components/NavBar/NavBar.tsx
+++ frontend/src/components/NavBar/NavBar.tsx
@@ -1,17 +1,14 @@
-import React from "react";
 import "./NavBar.css"
-// import 
-function Navbar() {
+
+const Navbar = () => {
     return (
         <div>
             <nav>
                 <ul>
-                    <li><a href="/">Landing</a></li>
-                    <li><a href="/home">Home</a></li>
-                    <li><a href="/course-info">Course Info</a></li>
-                    <li><a href="/course-compare">Course Compare</a></li>
-
-
+                    <li><a href="/">Landing🛬</a></li>
+                    <li><a href="/home">Home🏠</a></li>
+                    <li><a href="/course-info">Course Info🤔</a></li>
+                    <li><a href="/course-compare">Course Compare📖</a></li>
                 </ul>
             </nav>
         </div>
